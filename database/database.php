58db7a23c6cf7fc99c74155bebdc94219a3193e0
Irantwomiles <irantwomiles@gmail.com>
2018-12-16 12:44:46 -0500

    database info

--- database/database.php
+++ database/database.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "66.70.180.204";
+$username = "webapi";
+$password = "00TjJqwnIUL5qous";
 $database = "users";
 
 $connection = mysqli_connect($servername, $username, $password, $database);
