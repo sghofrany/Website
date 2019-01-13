f674d4a07f707320a8f414aa4279197707ad6af7
Irantwomiles <irantwomiles@gmail.com>
2019-01-12 21:51:16 -0500

    started work on footer

--- index.php
+++ index.php
@@ -7,56 +7,66 @@ require 'header.php';
 <head>
 	<title>PvPTemple</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
-    <div class="wrapper">
+    <div class="index-wrapper">
+        <div class="wrapper">
 
-        <div class="banner">
-            <label class="banner-text">pvptemple</label>
-        </div>
-
-        <div class="info-wrapper" id="one">
-
-            <div class="info">
-                <p class="info-title">OUR GOAL</p>
-                <p class="info-body">The worse hand disappears across the fiddle. Whatever vicar scatters the nervous outline. The cooling revenue denotes the arrow. A stroke institutes each alphabet.</p>
+            <div class="banner">
+                <label class="banner-text">pvptemple</label>
             </div>
-        
-        </div>
 
-        <div class="info-wrapper" id="two">
+            <div class="info-wrapper" id="one">
 
-            <div class="info">
-                <p class="info-title">MULTIPLE PROXIES</p>
-                <p class="info-body"> The worse hand disappears across the fiddle. Whatever vicar scatters the nervous outline. The cooling revenue denotes the arrow. A stroke institutes each alphabet.</p>
+                <div class="info">
+                    <p class="info-title">OUR GOAL</p>
+                    <p class="info-body">The worse hand disappears across the fiddle. Whatever vicar scatters the nervous outline. The cooling revenue denotes the arrow. A stroke institutes each alphabet.</p>
+                </div>
+            
             </div>
-        
-        </div>
 
-        <div class="info-wrapper" id="three">
+            <div class="info-wrapper" id="two">
 
-            <div class="info">
-                <p class="info-title">NETWORK SUPPORT</p>
-                <p class="info-body">The worse hand disappears across the fiddle. Whatever vicar scatters the nervous outline. The cooling revenue denotes the arrow. A stroke institutes each alphabet.</p>
+                <div class="info">
+                    <p class="info-title">MULTIPLE PROXIES</p>
+                    <p class="info-body"> The worse hand disappears across the fiddle. Whatever vicar scatters the nervous outline. The cooling revenue denotes the arrow. A stroke institutes each alphabet.</p>
+                </div>
+            
             </div>
-        
-        </div>
 
-        <div class="info-wrapper" id="four">
+            <div class="info-wrapper" id="three">
 
-            <div class="info">
-                <p class="info-title">THE STAFF TEAM</p>
-                <p class="info-body">The worse hand disappears across the fiddle. Whatever vicar scatters the nervous outline. The cooling revenue denotes the arrow. A stroke institutes each alphabet.</p>
+                <div class="info">
+                    <p class="info-title">NETWORK SUPPORT</p>
+                    <p class="info-body">The worse hand disappears across the fiddle. Whatever vicar scatters the nervous outline. The cooling revenue denotes the arrow. A stroke institutes each alphabet.</p>
+                </div>
+            
             </div>
-        
+
+            <div class="info-wrapper" id="four">
+
+                <div class="info">
+                    <p class="info-title">THE STAFF TEAM</p>
+                    <p class="info-body">The worse hand disappears across the fiddle. Whatever vicar scatters the nervous outline. The cooling revenue denotes the arrow. A stroke institutes each alphabet.</p>
+                </div>
+            
+            </div>
+            
         </div>
-        
+
+        <div class="footer">
+            <div class="wrapper">
+                <p>testing</p>
+
+            </div>
+        </div>
     </div>
-    <?php
-    //require 'footer';
-    ?>
+
+    <!--  -->
+
 </body>
 </html>
