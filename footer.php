73bb047a62f22558d2bc76cf8ccecec6c37ddac7
Irantwomiles <irantwomiles@gmail.com>
2019-01-30 18:36:49 -0500

    more footer work

--- footer.php
+++ footer.php
@@ -4,12 +4,24 @@
     </head>
 
     <body>
-        <div class="footer">
-            <div class="wrapper">
-                <p>testing</p>
+        
+    <footer>
+        <div class="wrapper">
+            <div class="footer-content">
+                <ul class="left-footer">
+                    <li>powered by <a href="#">conaxgames</a>.</li>
+                    <li>not affiliated with mojang ab.</li>
+                </ul>
 
+                <ul class="right-footer">
+                    <li>media</li>
+                    <li>rules</li>
+                    <li>terms of service</li>
+                </ul>
             </div>
+
         </div>
+    </footer>
     
     </body>
 </html>
